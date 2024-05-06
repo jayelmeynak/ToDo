f00ea6ed8369b5269a05a8f6f1e9a4b081384a68
magomed03 <magomed_mazhitov_03@mail.ru>
2024-05-06 03:50:06 +0300

    Add basic package and data class

--- app/src/main/java/com/example/todosum/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todosum/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.todosum
+package com.example.todosum.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.todosum.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
