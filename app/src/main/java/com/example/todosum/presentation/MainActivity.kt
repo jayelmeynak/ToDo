bbee69af5affb86fa127355956e0f495a0b850c2
magomed03 <magomed_mazhitov_03@mail.ru>
2024-05-07 19:11:06 +0300

    initialized viewmodel and recycle view.adapter in MainActivity

--- app/src/main/java/com/example/todosum/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todosum/presentation/MainActivity.kt
@@ -3,14 +3,38 @@ package com.example.todosum.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.todosum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var shopListAdapter: ShopListAdapter
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            shopListAdapter.submitList(it)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        with(binding) {
+            shopListAdapter = ShopListAdapter()
+            rvShopList.adapter = shopListAdapter
+            rvShopList.recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            rvShopList.recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
     }
 }
