da29dfbe6d1de9dfeb93ac37e9545a4ed1435a70
magomed03 <magomed_mazhitov_03@mail.ru>
2024-05-07 18:58:03 +0300

    Create data package and ShopListRepositoryImpl.class

--- app/src/main/java/com/example/todosum/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/todosum/data/ShopListRepositoryImpl.kt
@@ -1,34 +1,55 @@
 package com.example.todosum.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.todosum.domain.ShopItem
 import com.example.todosum.domain.ShopListRepository
+import kotlin.random.Random
 
-object ShopListRepositoryImpl: ShopListRepository {
-    private val shopList = mutableListOf<ShopItem>()
-    private var autoIncrementId = 1
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
+
+    private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 1000) {
+            val item = ShopItem(i,"Name $i", i, Random.nextBoolean())
+            addShopItem(item)
+        }
+    }
+
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == -1) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
-        val olditem = getShopItem(shopItem.id)
-        deleteShopItem(olditem)
-        shopList.add(shopItem)
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
-        return shopList.find { it.id == shopItemId
-        } ?: throw RuntimeException("Element with $shopItemId not found")
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toMutableList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 }
